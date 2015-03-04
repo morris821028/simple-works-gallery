c0128745f3ad2dd7c7c5f33d65edbd0e1391827b
Morris Yang <morris821028@gmail.com>
2015-03-04 09:40:55 +0800

    change structure & css design

--- README.md
+++ README.md
@@ -25,14 +25,40 @@ in header
 	...
       <script type="text/works-gallery">
 {
-  "works" : [
-      {
-        "title": "UVa Online Judge",
-        "cover": "http://i.imgur.com/y0Y2VRo.png",
-        "description": "Solved 2300+ problems <br/> Username: morris821028",
-        "link": ["http://uhunt.felix-halim.net/id/46705"],
-        "download": ["https://github.com/morris821028/UVa"]
-      }
+  "content" : [
+
+    { "title" : "School", 
+      "works" : [
+        {
+          "title": "Chat Room Application",
+          "cover": "http://i.imgur.com/oUO30I6.jpg",
+          "description": "Simple Chat Room with online game<br/> Socket programming practice (Java)",
+          "download": ["https://github.com/morris821028/hw-ChatRoom"],
+          "demo": [],
+          "video": ["https://www.youtube.com/watch?v=7ExCn1ipKeg"]
+        },
+        {
+          "title": "Fuzzy System",
+          "cover": "http://i.imgur.com/C44Hbrg.png",
+          "description": "Car simulation implements PSO, GA, and fuzzy system (Java)",
+          "download": ["https://github.com/morris821028/hw-FuzzySystem"],
+          "demo": [],
+          "video": ["https://www.youtube.com/watch?v=kt2mu679elU"]
+        }
+      ]
+    },
+    {
+      "title" : "ACMer",
+      "works" : [
+          {
+            "title": "UVa Online Judge",
+            "cover": "http://i.imgur.com/y0Y2VRo.png",
+            "description": "Solved 2800+ problems <br/> Username: morris821028",
+            "link": ["http://uhunt.felix-halim.net/id/46705"],
+            "download": ["https://github.com/morris821028/UVa"]
+          }
+      ]
+    }
   ]
 }
       </script>
--- js/script.js
+++ js/script.js
@@ -1,6 +1,39 @@
 $(document).ready(function() {
+	function getHtml(content, linkId) {
+		var html = '';
+		for (var i in content['works']) {
+			var item = content['works'][i],
+				tag = '',
+				screenshot = '',
+				inner = '',
+				links = '';
+			screenshot = '<div class="wg-screenshot wg-img-effect">';
+			screenshot = screenshot + '<img src="' + item.cover + '" class="wg-screenshot-img">';
+			screenshot = screenshot + '<span class="wg-preview-link">';
+			if (item.download) {
+				for (var j in item.download)
+					screenshot = screenshot + '<a href="' + item.download[j] + '" target="_blank"><i class="icon-download"></i></a>';
+			}
+			if (item.demo) {
+				for (var j in item.demo)
+					screenshot += '<a href="' + item.demo[j] + '" target="_blank"><i class="icon-eye"></i></a>';
+			}			
+			if (item.video) {
+				for (var j in item.video)
+					screenshot += '<a href="' + item.video[j] + '" target="_blank"><i class="icon-play"></i></a>';
+			}
+			screenshot = screenshot + '<span></div>';
+			inner = '<a  href="' + item.download[0] + '" class="wg-name" target="_blank">' + item.title + '</a>';
+			inner += '<p class="wg-desc">' + item.description + '</p>';
+
+			tag = screenshot + inner;
+			tag = '<li class="wg-item">' + tag + '</li>';
+			html += tag;
+		}
+		html = '<ul class="wg-container invisible" data-pos="' + linkId +  '">' + html + '</ul>';
+		return html;
+	}
 	$("script[type='text/works-gallery']").each(function(index, value) {
-		console.log(index);
 		var $container = $(this),
 			$widget,
 			content = $container.text(),
@@ -9,45 +42,38 @@ $(document).ready(function() {
 
 		content.trim();
 		content = JSON.parse(content);
-		var html = '';
-		for (var i in content['works']) {
-			var item = content['works'][i],
-				tag = '',
-				header = '',
-				inner = '',
-				links = '';
-			header = '<a class="image" style="background-image: url(' + item.cover + ')"></a>';
-			header = '<div class="header">' + header + '</div>';
-			inner = '<h3>' + item.title + '</h3><hr/>';
-			inner += '<p class="description">' + item.description + '</p>';
-			if (item.demo) {
-				for (var j in item.demo)
-					links += '<li><a href="' + item.demo[j] + '">Demo</a></li>';
-			}
-			if (item.video) {
-				for (var j in item.video)
-					links += '<li><a href="' + item.video[j] + '">Video</a></li>';
-			}
-			if (item.download) {
-				for (var j in item.download)
-					links += '<li><a href="' + item.download[j] + '">Download</a></li>';
-			}
-			if (item.link) {
-				for (var j in item.link)
-					links += '<li><a href="' + item.link[j] + '">Link</a></li>';
-			}
-			links = '<ul class="links">' + links + '</ul>';
-			inner += links;
-			inner = '<div class="inner">' + inner + '<div>';
-			tag = header + inner;
-			tag = '<li>' + tag + '</li>';
-			html += tag;
+		var html = '', selector = '', wglinkId = 0;
+		for (var i in content['content']) {
+			selector = selector + '<li data-target="wglinkId' + wglinkId + '">' + content['content'][i]['title'] + '</li>';
+			html = html + getHtml(content['content'][i], 'wglinkId' + wglinkId);
+			wglinkId++;
 		}
-		html = '<ul class="wg-item">' + html + '</ul>';
-		html = '<div class="wg-container"><hr/>' + html + '</div>';
-
+		selector = '<div class="wg-nav"><ul>' + selector + '</ul></div>';
+		html = '<div class="wg-wrapper">' + selector + html + '</div>';
 		$widget = $(html);
 
 		$container.after($widget);
+		var change_selector = (function() {
+			var prev;
+			return function(selector) {
+				if (prev) {
+					prev.removeClass('wg-nav-active');
+					var off = prev.attr('data-target');
+					$widget.find('[data-pos="' + off + '"]').addClass('invisible');
+				}
+				if (selector) {
+					selector.addClass('wg-nav-active');
+					var on = selector.attr('data-target');
+					$widget.find('[data-pos="' + on + '"]').removeClass('invisible');
+					prev = selector;
+				}
+			};
+		})();
+		var default_select = $widget.find('.wg-nav li:first');
+		change_selector(default_select);
+
+		$widget.find('.wg-nav li').click(function() {
+			change_selector($(this));
+		});
 	});
 });
